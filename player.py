c5670e4c04f463f7b5b7b8755d80f6d00c0ec825
ignacio cva <la_arena_y_yo@yahoo.com>
2021-01-13 10:26:26 +0100

    catch error if file doesnt exist #4

--- player.py
+++ player.py
@@ -28,12 +28,23 @@ class Player():
                 if self._currentComm != num:
                     self._currentComm = num
                     if self._name in decoded['extra_radios']:
-                        m.music.load("/media/mnt/" + decoded['tracks'][
-                            decoded['extra_radios'].index(self._name) % len(decoded['tracks'])
-                        ])
+                        try:
+                            track = decoded['tracks'][
+                                decoded['extra_radios'].index(self._name) % len(decoded['tracks'])]
+                            m.music.load("/media/mnt/" + track)
+                        except:
+                            print(f"music file {track} doesn't exists")
                     else:
-                        m.music.load("/media/mnt/" + decoded['tracks'][0])
-                    m.music.play()
+                        try: 
+                            track = decoded['tracks'][0]
+                            m.music.load("/media/mnt/" + track)
+                        except:
+                            print(f"music file {track} doesn't exists")
+                    try:
+                        m.music.play()
+                    except:
+                        print("didn't play...")
+                        
                     if debug:
                         print('playing', radio, num)
                     #self._playobj.wait_done()
